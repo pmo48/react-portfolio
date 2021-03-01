8501ca690a4f489c46bf395aaeae51b8968545fa
pmo48 <pmoriarty100@gmail.com>
2021-03-01 15:56:41 -0800

    resume page added

---
+++ react-portfolio/src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+// // import { Link } from "react-router-dom";
+// import "./style.css";
+
+// // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
+function Header() {
+  return (
+    <div class="page-header">
+        <h1 class="mt-3"></h1>
+    </div>
+  );
+}
+
+export default Header;
--- react-portfolio/src/pages/Resume.js
+++ react-portfolio/src/pages/Resume.js
@@ -1,33 +1,21 @@
 import React from "react";
+import cv from "../images/PatMoriartyResume.pdf"
+import cvimage from "../images/resume.png"
 
 function Resume() {
   return (
-    <div>
-        <h1>Resume!</h1>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-        <p>RESUME !</p>
-    </div>
+    <div className="row">
+        <div className="col-xl-8 text-white">
+          <div className="page-header">
+              <h1 className="mt-3">Resume</h1>
+          </div>
+          <div className="page-header">
+            <p className="text-white">Click image to download as PDF</p>
+        </div>
+        </div>
+        <a href={cv} download>
+          <img src={cvimage} className="img-fluid" alt="resume"/></a>
+      </div>
   );
 }
 
